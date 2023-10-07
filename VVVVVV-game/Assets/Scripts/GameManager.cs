28e7d9aeb910e5c21c9f96c67ef02246cd63534b
esterarpi11 <132432358+esterarpi11@users.noreply.github.com>
2023-10-07 17:13:28 +0200

    Main Menu design

--- VVVVVV-game/Assets/Scripts/GameManager.cs
+++ VVVVVV-game/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public enum GameState { Ready, Playing };
 
@@ -11,7 +12,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        SceneManager.LoadScene(sceneName: "MAINMENU");
     }
 
     // Update is called once per frame
