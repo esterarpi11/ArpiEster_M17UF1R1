9f312d9cdd899432c8edec400c40cd4dc1f0a725
esterarpi11 <132432358+esterarpi11@users.noreply.github.com>
2023-10-17 17:57:10 +0200

    Did quite a few levels

--- VVVVVV-game/Assets/Scripts/GameManager.cs
+++ VVVVVV-game/Assets/Scripts/GameManager.cs
@@ -35,9 +35,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape)) 
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            SceneManager.LoadScene(sceneBuildIndex: 0);
+            SceneManager.LoadScene(sceneName: "PAUSEMENU");
         }
     }
 }
